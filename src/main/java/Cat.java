641eed18ef75e8961820a848a219d344daa1a209
temkame <temkame@yandex.ru>
2022-03-28 23:26:44 +0300

    Commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String massage;
+
+    public String getMessage() {
+        return massage;
+    }
+
+    public void setMessage(String message) {
+        this.massage = message;
+    }
+}
